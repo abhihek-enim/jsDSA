ce5555567c4eea65aa5533f8e5c1cc579dc4209d
abhishek <shukla13122000@gmail.com>
2024-11-25 10:40:41 +0530

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,20 @@
+let x = -1221;
+let inputNum = x;
 
+let isLessThanZero = x < 0;
+if (isLessThanZero) {
+  x = -x;
+}
+
+let i = "";
+while (x > 0) {
+  let mod = x % 10;
+  i = i + mod;
+  x = Math.floor(x / 10);
+}
+i = Number(i);
+if (i == inputNum) {
+  console.log("true");
+} else {
+  console.log("false");
+}
